a58f906c25a5547f98bc223efb746dac487005cc
Jai Govindani <jai@honestbank.com>
2021-09-14 14:51:47 +0700

    ci(release): add semantic-release

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: [{ name: 'main' }],
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+};
