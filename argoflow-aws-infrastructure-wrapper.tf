98200aade583766febd8e172f6ab1468c47fc265
Jai Govindani <jai@honestbank.com>
2021-09-18 20:42:22 +0700

    feat(wrapper): add inputs and outputs

---
+++ argoflow-aws-infrastructure-wrapper.tf
@@ -0,0 +1,15 @@
+module "argoflow_aws_infrastructure" {
+  source = "./terraform"
+
+  aws_load_balancer_controller_namespace                  = var.aws_load_balancer_controller_namespace
+  aws_load_balancer_controller_service_account_name       = var.aws_load_balancer_controller_service_account_name
+  aws_region                                              = var.aws_region
+  aws_vpc_id                                              = var.aws_vpc_id
+  aws_vpc_private_subnets                                 = var.aws_vpc_private_subnets
+  aws_vpc_public_subnets                                  = var.aws_vpc_public_subnets
+  cluster_autoscaler_kubernetes_service_account_name      = var.cluster_autoscaler_kubernetes_service_account_name
+  cluster_autoscaler_kubernetes_service_account_namespace = var.cluster_autoscaler_kubernetes_service_account_namespace
+  eks_cluster_name                                        = var.eks_cluster_name
+  kubeflow_cluster_oidc_provider_arn                      = var.kubeflow_cluster_oidc_provider_arn
+  stage                                                   = var.stage
+}
---
+++ inputs.tf
@@ -0,0 +1,61 @@
+# ###################################################################################################################
+# argoflow-aws-infrastructure input variables
+# ###################################################################################################################
+
+variable "aws_load_balancer_controller_namespace" {
+  type        = string
+  description = "The Kubernetes Namespace containing the aws-load-balancer-controller KSA (Kubernetes Service Account)"
+  default     = "kube-system"
+}
+
+variable "aws_load_balancer_controller_service_account_name" {
+  type        = string
+  description = "The name of the aws-load-balancer-controller KSA (Kubernetes Service Account)"
+  default     = "aws-load-balancer-controller"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region in which to build resources"
+}
+
+variable "aws_vpc_id" {
+  type        = string
+  description = "The VPC ID where the argoflow-aws instance will be located"
+}
+
+variable "aws_vpc_private_subnets" {
+  type        = list(string)
+  description = "A list of the private VPC subnet IDs used by the Kubeflow EKS cluster"
+}
+
+variable "aws_vpc_public_subnets" {
+  type        = list(string)
+  description = "A list of the public VPC subnet IDs used by the Kubeflow EKS cluster"
+}
+
+# Kubernetes Cluster Autoscaler: https://github.com/kubernetes/autoscaler
+# Cluster Autoscaler YAML: https://raw.githubusercontent.com/kubernetes/autoscaler/master/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml
+
+variable "cluster_autoscaler_kubernetes_service_account_name" {
+  description = "The Kubernetes Service Account used by the cluster-autoscaler pod"
+  default     = "cluster-autoscaler"
+}
+
+variable "cluster_autoscaler_kubernetes_service_account_namespace" {
+  description = "The Kubernetes Namespace in which the cluster-autoscaler Service Account is located"
+  default     = "kube-system"
+}
+
+variable "eks_cluster_name" {
+  type        = string
+  description = "The name of the EKS (Elastic Kubernetes Service) cluster."
+}
+
+variable "kubeflow_cluster_oidc_provider_arn" {
+  description = "The OIDC provider ARN of the Kubeflow Kubernetes cluster"
+}
+
+variable "stage" {
+  description = "The stage (environment) of the build - usually one of [test, dev, qa, prod]"
+}
